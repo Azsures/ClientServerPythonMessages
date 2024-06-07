ceaac1120ed81236743b1e7dcf562337b1914a94
Magnus <87209706+Azsures@users.noreply.github.com>
2024-06-06 21:53:28 -0300

    Add files via upload

--- client2.py
+++ client2.py
@@ -2,14 +2,14 @@ import socket
 import threading
 from time import sleep
 
-HOST = socket.gethostbyname(socket.gethostname())  # The server's IP address
+HOST = socket.gethostbyname(socket.gethostname())  # The server's hostname or IP address
 PORT = 35567  # The port used by the server
 
 
 def send_message(s):
     while True:
         message = input(">> ")
-        message = f"{message}||10.100.0.83:34560"
+        message = f"{message}||10.100.0.83:5556"
         s.send(message.encode('utf8'))
 
 def receive_message(s):
@@ -17,11 +17,14 @@ def receive_message(s):
         data = s.recv(1024).decode('utf8')
         print(f"\n<< {data}\n>> ", end="")
 
+
+
 s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.bind((socket.gethostbyname(socket.gethostname()), 35580))
-s.bind(('10.100.0.85', 35580))
-s.connect((HOST, PORT))
+s.bind((socket.gethostbyname(socket.gethostname()), 34560))
+s.connect((socket.gethostbyname(socket.gethostname()), 35567))
+#s.connect(('10.100.0.83', 5555))
 receive = threading.Thread(target=receive_message, args=(s,))
 receive.start()
 send    = threading.Thread(target=send_message, args=(s,))
 send.start()
+    
--- server.py
+++ server.py
@@ -2,44 +2,43 @@ import socket
 import threading
 import sys
 
+def client_handle(conn, addr, list_of_socks):
+    while True:
+        data = conn.recv(1024).decode('utf8')
+        if not data or data.find('||') == -1:
+            break
+        message = data.split('||')[0]
+        dest_socket = data.split('||')[1].split(':')
+        print(f"\nReceived message: {message}\nDestination: {dest_socket[0]}:{dest_socket[1]}")
 
 
-def client_handle(conn, addr):
-    data = conn.recv(1024).decode('utf8')
-    message = data.split('||')[0]
-    dest_socket = data.split('||')[1].split(':')
-    print(f"\nReceived message: {message}\nDestination: {dest_socket[0]}:{dest_socket[1]}")
-
-    try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((dest_socket[0], int(dest_socket[1])))
-            s.send(message.encode('utf8'))
-            if(s.recv(1024).decode('utf8') == "sucess"):
-                conn.send(b"Sucess")
-    except:
-        conn.send(b"error connecting to destination")
-        print("error connecting")
+            for sockt in list_of_socks:
+                print(sockt)
+                if sockt[1][0] == dest_socket[0] and sockt[1][1] == int(dest_socket[1]):
+                    sockt[0].send(message.encode('utf8'))
+            #s.connect((dest_socket[0], int(dest_socket[1])))
+            #conn.send(message.encode('utf8'))
+            #if(s.recv(1024).decode('utf8') == "sucess"):
+                #   conn.send(b"Sucess")
+
+            
     
 def serve():
-    HOST = "192.168.88.62"
+    HOST = socket.gethostbyname(socket.gethostname())
     PORT = 35567
+    list_of_sockets = []
 
-    try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-            sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-            sock.bind((HOST, PORT))
-            sock.listen(3)
-            while True:
-                print(f"\nPre Active Threads: {threading.active_count()}")
-                conn, addr = sock.accept()
-                threading.Thread(target=client_handle, args=(conn, addr)).start()
-                print(f"\nActive Threads: {threading.active_count()}")
-    except Exception as ex:
-        print(ex)
-    except KeyboardInterrupt as ex:
-        print(ex)
-    except:
-        print(sys.exec_info())
+    sock= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((HOST, PORT))
+    print(HOST, PORT)
+    sock.listen(10)
+    while True:
+        conn, addr = sock.accept()
+        list_of_sockets.append([conn, addr])
+        threading.Thread(target=client_handle, args=(conn, addr, list_of_sockets)).start()
+
 
 if __name__ == "__main__":
     serve()
